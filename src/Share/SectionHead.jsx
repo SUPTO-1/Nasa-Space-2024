03ddc8991f7de2ed7474b99e1c029c9ac47d993c
Shahriyar <chyshahriyar@gmail.com>
2024-10-03 12:02:50 +0600

    Our Team section and Nav & Footer functionality completed

---
+++ src/Share/SectionHead.jsx
@@ -0,0 +1,16 @@
+
+
+const SectionHead = () => {
+    return (
+        <>
+            <div className="relative text-center py-14">
+                <h1 className="uppercase text-6xl md:text-8xl lg:text-9xl text-[#6c757d] font-bold opacity-30 text-center">Our Team</h1>
+                <p className="lg:text-4xl md:text-2xl text-lg font-bold absolute lg:bottom-[84px] md:bottom-[80px] bottom-[69px] text-center w-full uppercase text-[#FFFFFF]">Astro Avaengers
+                    <span className="border-2 bottom-5 block border-[#20c997] lg:w-32 md:w-24 w-14 mx-auto lg:mt-2 md:mt-1"></span>
+                </p>
+            </div>
+        </>
+    );
+};
+
+export default SectionHead;
--- src/component/OurTeam.jsx
+++ src/component/OurTeam.jsx
@@ -1,37 +1,100 @@
-// import jayed from '../../public/Sheikh Jayed Uddin.jpg'
+import jayed from '../../public/Sheikh Jayed Uddin.jpg'
 import supto from '../../public/Supto.jpg'
-// import samea from '../../public/Samea.jpg'
+import samea from '../../public/Samea.jpg'
 import { Link } from 'react-router-dom';
-import { ImFacebook2 } from 'react-icons/im';
-import { FaGithub, FaLinkedinIn } from 'react-icons/fa';
-// import jerin from '../../public/Sheikh Jayed Uddin.jpg'
-// import hridoy from '../../public/Sheikh Jayed Uddin.jpg'
+import { FaFacebookF, FaGithub, FaLinkedinIn } from 'react-icons/fa';
+import jerin from '../../public/Jerin.jpg'
+import hridoy from '../../public/shahriyar.jpg'
+import SectionHead from '../Share/SectionHead';
 
 const OurTeam = () => {
     return (
         <>
-            <section className="py-6 dark:bg-gray-100 dark:text-gray-800">
-                <div className="container flex flex-col items-center justify-center p-4 mx-auto space-y-8 sm:p-10">
-                    <h1 className="text-4xl font-bold leading-none text-center sm:text-5xl">Our team</h1>
-                    <p className="max-w-2xl text-center dark:text-gray-600">At a assumenda quas cum earum ut itaque commodi saepe rem aspernatur quam natus quis nihil quod, hic explicabo doloribus magnam neque, exercitationem eius sunt!</p>
-                </div>
-                <div className="">
-                    <div className="">
-                        <div className='rounded-full h-56 w-56 p-1 border-4'>
-                            <img alt="" className="object-cover h-52 w-52 mx-auto bg-center dark:bg-gray-500 rounded-full" src={supto} />
-                        </div>
-                        <div className="flex flex-col">
-                            <h4 className="text-xl font-semibold">Md Jahidul Islam Supta</h4>
-                            <p className="text-sm dark:text-gray-600">Team Leader</p>
-                            <div className="flex mt-2 space-x-2">
-                                <Link><h1><ImFacebook2 /></h1></Link>
-                                <Link><h1><FaLinkedinIn /></h1></Link>
-                                <Link><h1><FaGithub /></h1></Link>
+            <div className="bg-[#343a40]">
+                <SectionHead></SectionHead>
+                <section className="dark:bg-gray-100 dark:text-gray-800 pb-16 text-[#FFFFFF]">
+                    {/* Team Leader */}
+                    <div className="mb-8">
+                        <div className="border-2 border-[#7b8996] rounded-xl w-[350px] mx-auto py-6 px-3">
+                            <div className=''>
+                                <img alt="" className="object-cover h-60 w-60 mx-auto bg-center dark:bg-gray-500 rounded-full" src={supto} />
+                            </div>
+                            <div className="flex flex-col items-center mt-3">
+                                <h4 className="text-2xl font-bold">Md Jahidul Islam Supta</h4>
+                                <p className="text-base dark:text-gray-600 mt-2">Team Leader</p>
+                                <div className="flex mt-2 space-x-2 text-lg">
+                                    <Link><h1><FaFacebookF /></h1></Link>
+                                    <Link><h1><FaLinkedinIn /></h1></Link>
+                                    <Link><h1><FaGithub /></h1></Link>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
-            </section>
+                    {/* Team Member */}
+                    <div className="mx-3 grid grid-cols-4">
+                        {/* Jayed */}
+                        <div className="border-2 border-[#7b8996] rounded-xl w-[350px] mx-auto py-6 px-3">
+                            <div className=''>
+                                <img alt="" className="object-cover h-60 w-60 mx-auto bg-center dark:bg-gray-500 rounded-full" src={jayed} />
+                            </div>
+                            <div className="flex flex-col items-center mt-3">
+                                <h4 className="text-2xl font-bold">Sheikh Jayed Uddin</h4>
+                                <p className="text-base dark:text-gray-600 mt-2">Developer</p>
+                                <div className="flex mt-2 space-x-2 text-lg">
+                                    <Link><h1><FaFacebookF /></h1></Link>
+                                    <Link><h1><FaLinkedinIn /></h1></Link>
+                                    <Link><h1><FaGithub /></h1></Link>
+                                </div>
+                            </div>
+                        </div>
+                        {/* Hridoy */}
+                        <div className="border-2 border-[#7b8996] rounded-xl w-[360px] mx-auto py-6 px-3">
+                            <div className=''>
+                                <img alt="" className="object-cover h-60 w-60 mx-auto bg-center dark:bg-gray-500 rounded-full" src={hridoy} />
+                            </div>
+                            <div className="flex flex-col items-center mt-3">
+                                <h4 className="text-xl font-bold">Md. Shahriyar Hossain Chowdhury</h4>
+                                <p className="text-base dark:text-gray-600 mt-2">Developer</p>
+                                <div className="flex mt-2 space-x-2 text-lg">
+                                    <Link><h1><FaFacebookF /></h1></Link>
+                                    <Link><h1><FaLinkedinIn /></h1></Link>
+                                    <Link><h1><FaGithub /></h1></Link>
+                                </div>
+                            </div>
+                        </div>
+                        {/* Jerin */}
+                        <div className="border-2 border-[#7b8996] rounded-xl w-[350px] mx-auto py-6 px-3">
+                            <div className=''>
+                                <img alt="" className="object-cover h-60 w-60 mx-auto bg-center dark:bg-gray-500 rounded-full" src={jerin} />
+                            </div>
+                            <div className="flex flex-col items-center mt-3">
+                                <h4 className="text-2xl font-bold">Meherun Nesa Jerin</h4>
+                                <p className="text-base dark:text-gray-600 mt-2">UI/UX Designer</p>
+                                <div className="flex mt-2 space-x-2 text-lg">
+                                    <Link><h1><FaFacebookF /></h1></Link>
+                                    <Link><h1><FaLinkedinIn /></h1></Link>
+                                    <Link><h1><FaGithub /></h1></Link>
+                                </div>
+                            </div>
+                        </div>
+                        {/* Samea */}
+                        <div className="border-2 border-[#7b8996] rounded-xl w-[350px] mx-auto py-6 px-3">
+                            <div className=''>
+                                <img alt="" className="object-cover h-60 w-60 mx-auto bg-center dark:bg-gray-500 rounded-full" src={samea} />
+                            </div>
+                            <div className="flex flex-col items-center mt-3">
+                                <h4 className="text-2xl font-bold">Samea Binte Saif</h4>
+                                <p className="text-base dark:text-gray-600 mt-2">Data Analyst</p>
+                                <div className="flex mt-2 space-x-2 text-lg">
+                                    <Link><h1><FaFacebookF /></h1></Link>
+                                    <Link><h1><FaLinkedinIn /></h1></Link>
+                                    <Link><h1><FaGithub /></h1></Link>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            </div>
         </>
     );
 };
